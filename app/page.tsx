be86e52bae0c0f1847bb6461b4e5a2d21d8edc61
mahjabin-mim <mahjabinmim711@gmail.com>
2026-03-29 16:54:17 +0600

    SSR added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import BlogCard from "@/components/BlogCard";
 import { API_URL } from "@/lib/api";
 import axios from "axios";
 
+export const dynamic = "force-dynamic";
+ 
 type Blog = {
   id: number;
   title: string;
